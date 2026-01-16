54cc1b64f65487657132b3072aa86a034235d14c
Thomas AROCENA <arocena.thomas@gmail.com>
2026-01-16 11:34:56 +0100

    add gsdesign comparisons

--- R/rpact_wrapper.R
+++ R/rpact_wrapper.R
@@ -93,35 +93,7 @@ rpact_wrapper <- function(
 
 
 
-#' Wrapper around rpact's `getSampleSizeSurvival` with Group Sequential design.
-#'
-#' @param alpha Type I error, a numerical value in ]0, 1[.
-#' @param power Power, a numerical value in ]0, 1[. Represent (1 - beta),
-#' where beta is the Type II error.
-#' @param hr Hazard ratio. a numerical value in ]0, 1[.
-#' Represent the fraction of lambdas from two groups, saying if a group
-#' has better or worse survival probability.
-#' @param surv_t Survival probability of control group at event time t.
-#' Default value for `event_time` is 3.
-#' So without event time specification, correspond to 3-year survival.
-#' @param event_time Time at which we evaluate survival probability. Default is 3.
-#' @param accrual_time Time of accrual. Default is 3.
-#' @param follow_up_time (Minimal) follow-up time. Default is 3.
-#' Study duration correspond to `accrual_time` + `follow_up_time`.
-#' @param sided One of c(1, 2). Is the test one-sided or two-sided. Default is 2
-#' @param computation One of "c("Schoenfeld", "Freedman", "HsiehFreedman")".
-#' @param allocation_ratio Allocation ratio of sample size between control and testing group (Test/control).
-#' Default is 1 corresponding to same allocation in control and testing group.
-#' @param dropout_rate_1 Rate of droupout in control group. Default is 0.
-#' @param dropout_rate_2 Rate of droupout in testing group. Default is 0.
-#' @param error What should be returned for needed number of events and sample size
-#' when an error occured. Default is `NA_real_`.
-#'
-#' @returns A tibble with e = number of needed event and n = required sample size.
-#'
-#' @references
-#' The formula of Kim & Tsiatis (Biometrics, 1990) is used to calculate the expected number of events
-#' under the alternative (see also Lakatos & Lan, Statistics in Medicine, 1992).
+
 rpact_gs_wrapper <- function(
   alpha,
   power,
