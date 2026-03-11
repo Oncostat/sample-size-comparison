401342b24993fc9ecce93cec7dd9fa417d6c0a51
Thomas AROCENA <arocena.thomas@gmail.com>
2026-03-11 17:33:32 +0100

    refactor/feat: everythings now in /site-files
    - corrected some reviewers comment:
      * add some references
      * better interactivity in /interactive
      * lots of small things... >: )

---
+++ site-files/additional-content/additional-content-setup.R
@@ -0,0 +1,21 @@
+#* SETUP FILE FOR ADDITIONAL-CONTENT QMD
+
+suppressPackageStartupMessages(suppressWarnings({
+  library(tidyverse)
+  library(gt)
+  library(S7)
+  library(plotly)
+  library(crosstalk)
+  library(bslib)
+  library(bsicons)
+  library(DT)
+}))
+
+# Source S7 classes and methods
+# source("../../R/S7/generics.R")
+# source("../../R/S7/ssc_design.R")
+# source("../../R/S7/ssc_results.R")
+
+# Theming 
+source("../theming_flatly.R")
+source("../theming_darkly.R")
